e23eb50861dabe9f960e009d3474b5ee7b6cfa38
Ruchika Agarwal <ruchika.kharwar@gmail.com>
2015-04-06 23:22:59 +0000

    This adds in the unique name function, when the ingredient list is created it is associated with a unique name and looked up every time, the bartender asks for drinks in a loop

--- bartender.py
+++ bartender.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 #Questions to ask each customer or after every drink
+
 questions = {
     "strong": "Do ye like yer drinks strong?",
     "salty": "Do ye like it with a salty tang?",
@@ -33,15 +34,63 @@ def ask_drink(questions):
   print customer_drink  
   return customer_drink
 
+adj = ["cuddly", "furry", "yellow","dirty","bloody","hairy","naughty"]
+noun = ["seadog", "tiger","puppy", "tarantula", "sealion","hippo","tuna","shark"]
+name_dir = {}
+
+def get_uniquedrinkname(i_list):
+  name = random.choice(adj)+" "+random.choice(noun)
+  while name in name_dir:
+    name = random.choice(adj)+" "+random.choice(noun)
+  #Add to the name directory
+  name_dir[name] = i_list
+  print "Name director is now {} ".format(name_dir)
+  return name
+  
+def getdrinkname(i_list):
+  #Search the ingredients list in the name directory
+  print "Getting drink name "
+  for name, ing in name_dir.iteritems():
+    if ing == i_list:
+      return name
+  newname = get_uniquedrinkname(i_list)
+  return newname
+
 def main():
+  another = False
   c_drink = {}
   ing_list = []
   c_drink = ask_drink(questions)
   ing_list = construct_drink(ingredients, c_drink)
+  
   if ing_list:
-    print "Your drink contains all this cool stuff \n" 
+#   Create name and add it to the name dictionary
+    drinkname = getdrinkname(ing_list)
+    print "\nYour drink {} contains all this cool stuff \n".format(drinkname) 
     for ing in ing_list:  
       print "{}".format(ing)
+
+  print "Would you like another ? "    
+  another = raw_input().lower() in ['y','yes']
+    
+  while another:
+    print "Would you like the same as the last or a new one ? "
+    same = raw_input().lower() in ['s', 'same']
+    if same:
+      drinkname = getdrinkname(ing_list)
+      print " {} coming up .. ".format(drinkname)
+    else:
+      c_drink = {}
+      ing_list = []
+      c_drink = ask_drink(questions)
+      ing_list = construct_drink(ingredients, c_drink)
+      if ing_list:
+        print "Your drink contains all this cool stuff \n" 
+        for ing in ing_list:  
+          print "{}".format(ing)
+    print "Would you like another ? "    
+    another = raw_input().lower() in ['y','yes']
+    
   
 if __name__ =='__main__':
   main()                    
